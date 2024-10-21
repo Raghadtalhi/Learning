84948d1bdac68d5110bebf80df5e0d3e072554ba
Raghad <ragadtalhi@gmail.com>
2024-10-21 09:21:32 +0300

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Raghad Altalhi on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
